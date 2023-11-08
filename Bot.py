ba760c9e0dad64f74565cf6646cda6cf6a3ec2da
Ahek <46721468+Ahek@users.noreply.github.com>
2023-11-08 12:01:04 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -33,7 +33,6 @@ class MyBot:
         Ideally you'll be using this one to access your animelist, but this only works if you made your animelist public
         '''
         mal_access_token = 'PUT YOUR ACCESS TOKEN HERE'
-        url = 'https://api.myanimelist.net/v2/users/@me/animelist'
         headers = {
           'Authorization': f'Bearer {mal_access_token}',
         }
